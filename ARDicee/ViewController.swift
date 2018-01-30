17fddcbf983204d1620cc192934f957e19e8fc92
Cullen Mooney <cullen@protonmail.com>
2018-01-29 21:22:03 -0600

    horizontal planes now detected

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // this shows us the feature points that are looking for plane detection
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // units of measure are meters!
         //let cube = SCNBox (width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
@@ -47,16 +50,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+//
+//        // the name is the identity in the attributes tab
+//        // recursively --> goes down the tree to find the name
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//
+//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//
+//            sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
         
-        // the name is the identity in the attributes tab
-        // recursively --> goes down the tree to find the name
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
         
-            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-        
-            sceneView.scene.rootNode.addChildNode(diceNode)
-        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -64,6 +69,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        // allows us to place dice on a horizontal plane
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -74,6 +82,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    // when it detects a horizontal plane, it will call this method
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // we want to check if the anchor is a horizontal plane
+        if anchor is ARPlaneAnchor {
+            //if the anchor is of type ARplaneanchor then we will change its type to that
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            // only use x and z for this method
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            // our plane is currently vertical and we need to rotate 90deg to make it horizontal
+            // -float.pi/2 is the radians of 90degrees clockwise, not counterclockwise
+            // we want to rotate along x axis so it is 1
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named : "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+        } else {
+            return
+        }
     }
     
 }
